c5161cb3835136e518b047337df8922c842bf5e3
ApostoL <ApostoL>
2017-06-10 22:19:53 +0600

    created input valid app

---
+++ js/app.js
@@ -0,0 +1,50 @@
+var Input = React.createClass({
+	getInitialState: function () {
+		return ({
+			value: ''
+		});
+	},
+
+	onInputChange: function (e) {
+		this.setState({
+			value: e.target.value
+		});
+	},
+
+	filterInputValue: function () {
+
+		var	i = 0,
+				newInputValue,
+				newInputValueArray,
+				currentInputValue = this.state.value,
+				filteredValueArray = currentInputValue.split('');
+
+		newInputValueArray = filteredValueArray.filter(function(number) {
+			number = +number;
+			if (number != NaN) return number;
+		});
+
+		newInputValue = newInputValueArray.join('');
+
+		this.setState({
+			value: newInputValue
+		})
+	},
+
+	render: function () {
+		return (
+			<div>
+				<input placeholder='Type something...'
+							 onBlur={this.filterInputValue}
+							 value={this.state.value}
+							 onChange={this.onInputChange}
+				/>
+			</div>
+		);
+	}
+});
+
+ReactDOM.render(
+	<Input />,
+	document.getElementById('root')
+);
